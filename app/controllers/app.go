b75b56d2eece9bc85d9300f3400cedd2b7fd9420
Goncalo Amaral <goa.docs@gmail.com>
2018-07-16 17:58:41 +0100

    Started controllers

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"search_tree/tree"
+	"strings"
+
 	"github.com/revel/revel"
 )
 
@@ -8,6 +11,36 @@ type App struct {
 	*revel.Controller
 }
 
-func (c App) Index() revel.Result {
-	return c.Render()
+var Root *tree.Node
+
+func (c App) DataPage() revel.Result {
+
+}
+
+func (c App) Data() revel.Result {
+	if Root == nil {
+		Root = tree.CreateNode()
+	}
+
+	input := c.Params.Query.Get("data")
+
+	words := strings.Split(input, " ")
+
+	for _, word := range words {
+		Root.AddWord(word)
+	}
+
+	c.Redirect("/search")
+}
+
+func (c App) SearchPage(request string) revel.Result {
+
+}
+
+func (c App) Search(request string) revel.Result {
+	input := c.Params.Query.Get("data")
+
+	suggestions := Root.GetSuggestions(input)
+
+	c.Render(suggestions)
 }
